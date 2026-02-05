9f38d409e5a63dafeab4a9e7fc057bb9a8035996
rwqfsfasxc100 <123430291+rwqfsfasxc100@users.noreply.github.com>
2026-02-05 09:35:45 -0800

    Update RPCNetworking.gd

--- networking/RPCNetworking.gd
+++ networking/RPCNetworking.gd
@@ -123,11 +123,13 @@ func set_end_timer(time:int = 0,do_update = false):
 
 func set_state(text:String,do_update = false):
 	if connected:
-		rpc("set_state",TranslationServer.translate(text),do_update)
+		text = TranslationServer.translate(text)
+		rpc("set_state",text,do_update)
 
 func set_details(text:String,do_update = false):
 	if connected:
-		rpc("set_details",TranslationServer.translate(text),do_update)
+		text = TranslationServer.translate(text)
+		rpc("set_details",text,do_update)
 
 func update_rpc():
 	if connected:
