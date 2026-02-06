18e649587cb6ce77abcc55de450686bf682b3211
rwqfsfasxc100 <123430291+rwqfsfasxc100@users.noreply.github.com>
2026-02-05 17:49:04 -0800

    Update SimulationLayer.gd

--- enceladus/SimulationLayer.gd
+++ enceladus/SimulationLayer.gd
@@ -1,2 +1,12 @@
 extends "res://enceladus/Simulator/SimulationLayer.gd"
 
+onready var rpc = get_tree().get_root().get_node("DiscordRPC_Network")
+
+signal simulationEnd(scene)
+
+func _ready():
+	connect("simulation",rpc,"loader_changed",["enceladus","simulator"])
+	connect("simulationEnd",rpc,"loader_changed")
+func clearSimulation():
+	.clearSimulation()
+	emit_signal("simulationEnd","enceladus")
